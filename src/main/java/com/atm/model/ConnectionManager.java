8d0942ab83d19f9e3c105ae592aa3063994a5ee3
MikLay <mixei112299@gmail.com>
2019-11-21 19:09:08 +0200

    [LOGGING] Configure log4j

--- src/main/java/com/atm/model/ConnectionManager.java
+++ src/main/java/com/atm/model/ConnectionManager.java
@@ -1,6 +1,6 @@
 package com.atm.model;
 
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -9,57 +9,57 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@Log4j
 public class ConnectionManager {
-    private static final Logger LOGGER = Logger.getLogger(ConnectionManager.class.getSimpleName());
 
     private static Connection connection = null;
     private static DataSource dataSource = null;
 
     public static Connection getConnection() {
-        LOGGER.info("Method getConnection start");
+        log.info("Method getConnection start");
         try {
             dataSource = (DataSource) new InitialContext().lookup("jdbc/gymnetwork");
             connection = dataSource.getConnection();
         } catch (NamingException e) {
-            LOGGER.error("getConnection caught NamingException");
-            LOGGER.trace(e);
+            log.error("getConnection caught NamingException");
+            log.trace(e);
             e.printStackTrace();
         } catch (SQLException sqlE) {
-            LOGGER.error("getConnection caught SQLException");
-            LOGGER.trace(sqlE);
+            log.error("getConnection caught SQLException");
+            log.trace(sqlE);
             sqlE.printStackTrace();
         }
 
-        LOGGER.info("getConnection end; return: " + connection);
+        log.info("getConnection end; return: " + connection);
         return connection;
     }
 
     public static DataSource getModelConnection() {
-        LOGGER.info("getModelConnection start");
+        log.info("getModelConnection start");
 
         try {
             Context context = (Context) new InitialContext().lookup("java::comp/env");
             dataSource = (DataSource) context.lookup("jdbc/gymnetwork");
         } catch (NamingException e) {
-            LOGGER.error("getModelConnection caught NamingException");
-            LOGGER.trace(e);
+            log.error("getModelConnection caught NamingException");
+            log.trace(e);
             e.printStackTrace();
         }
 
-        LOGGER.info("modelConnection end; return: " + dataSource);
+        log.info("modelConnection end; return: " + dataSource);
         return dataSource;
     }
 
 
     public static void closeConnection() {
-        LOGGER.info("closeConnection start");
+        log.info("closeConnection start");
         if (connection != null) {
-            LOGGER.info("connection not null");
+            log.info("connection not null");
             try {
                 connection.close();
             } catch (SQLException sqlE) {
-                LOGGER.error("closeConnection caught SQLException ");
-                LOGGER.trace(sqlE);
+                log.error("closeConnection caught SQLException ");
+                log.trace(sqlE);
                 sqlE.printStackTrace();
             }
         }
